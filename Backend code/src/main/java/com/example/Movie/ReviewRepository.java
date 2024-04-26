8cd2f8f692b5d83195ad3f4338708f913770a5a4
Bharath-6 <67066997+Bharath-6@users.noreply.github.com>
2024-04-26 23:35:49 +0530

    Done with Backend and Frontend

---
+++ Backend code/src/main/java/com/example/Movie/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.example.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ Backend code/src/main/java/com/example/Movie/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.Movie;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow requests from localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                .allowedHeaders("*") // Allowed headers
+                .allowCredentials(true); // Allow credentials such as cookies (if applicable)
+    }
+}
